98c44d79d8bbcd6d1dc4004064e3aa2d885af422
zaldyprtm <zaldyprtm@gmail.com>
2024-04-05 13:59:23 +0700

    fix modal box

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -8,11 +8,11 @@ const Modal = ({ toggleModal }) => {
   return (
     <>
       <div className="fixed top-0 mx-auto w-[400px] h-full flex justify-center items-center bg-gray-800 bg-opacity-50 z-50 bg-transparent">
-        <div className="bg-white p-8 rounded-lg shadow-md">
+        <div className="bg-white p-8 w-[350px] rounded-3xl shadow-md">
           <h2 className="text-xl uppercase text-black font-bold mb-4">Tentang kedai kami</h2>
-          <p className="text-gray-700">Halo.....☕</p>
-          <p className="text-gray-700">selamat menikmati menu di kedai kami</p>
-          <p className="text-gray-700">disini kami menyediakan berbagai menu macam fresh drink yang segar.</p>
+          <p className="text-gray-700">Halo Folks.....☕</p>
+          <p className="text-gray-700">Selamat menikmati menu di kedai kami</p>
+          <p className="text-gray-700">disini kami menyediakan berbagai macam menu fresh drink yang segar.</p>
           <p className="text-gray-700">jangan lupa order menu kami yaa....😎</p>
           
           <button
