3d1af21af5145b0443b9176aa05d7858bbe22440
zaldyprtm <zaldyprtm@gmail.com>
2024-04-07 13:15:29 +0700

    add input nma

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,22 @@ import React, { useState } from 'react';
 import './App.css';
 import './index.css';
 import Footer from './components/Footer';
-import Header from './components/Header'
+import Header from './components/Header';
 import Slide from './components/Slide';
 import FoodAndDrinkList from './components/FoodAndDrinkList';
 
-
 const App = () => {
-  // const [cartItems, setCartItems] = useState([]);
-  const [showModalPesanan, setShowModalPesanan] = useState(false); // State untuk menunjukkan apakah modal pesanan ditampilkan
+  const [userName, setUserName] = useState(""); // State untuk menyimpan nama pengguna
+  const [inputName, setInputName] = useState(""); // State untuk menyimpan input nama sementara
 
-  const openModalPesanan = () => {
-    setShowModalPesanan(true); // Set state menjadi true ketika ingin menampilkan modal
+  // Fungsi untuk menangani perubahan pada input nama
+  const handleNameChange = (event) => {
+    setInputName(event.target.value); // Mengupdate state inputName
   };
 
-  const closeModalPesanan = () => {
-    setShowModalPesanan(false); // Set state menjadi false ketika ingin menyembunyikan modal
+  // Fungsi untuk menangani submit nama
+  const handleSubmit = () => {
+    setUserName(inputName.trim()); // Mengupdate state userName dengan nama yang diinput
   };
 
   return (
@@ -24,8 +25,24 @@ const App = () => {
       <div>
         <Header />
         <Slide />
-        <FoodAndDrinkList
-        />
+        {/* Form untuk input nama */}
+        {!userName && (
+          <div className="mt-4 px-4">
+            <label htmlFor="userName" className="block mb-2">Haloo selamat datang di kedai kami😎 sebelum memesan pastikan masukan nama kamu yaa...😁</label>
+            <input
+              type="text"
+              id="userName"
+              value={inputName}
+              onChange={handleNameChange}
+              className="px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
+            />
+            {/* Tombol submit */}
+            <button onClick={handleSubmit} className="px-4 py-2 bg-indigo-500 text-white rounded-md mt-2 hover:bg-indigo-600">Submit Nama</button>
+          </div>
+        )}
+        {/* Pesan selamat datang setelah nama di-submit */}
+        {userName && <p className='mt-4 px-4 font-semibold '>Halo, {userName}! 😎, yuk berbelanja di kedai kami dan nikmati berbagai menu pilihan.</p>}
+        <FoodAndDrinkList />
       </div>
       <div></div>
       <div>  
